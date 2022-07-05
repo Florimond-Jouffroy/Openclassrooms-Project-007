cbf6406925d20cd6e394ad3ce576614942d1cbd2
Florimond Jouffroy <florimond.jouffroy@gmail.com>
2022-07-05 12:11:43 +0200

    feat: add user fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,26 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager): void
-    {
-        // $product = new Product();
-        // $manager->persist($product);
+  public function __construct(private UserPasswordHasherInterface $passwordHashe)
+  {
+  }
 
-        $manager->flush();
-    }
+  public function load(ObjectManager $manager): void
+  {
+
+    $user = new User;
+    $user
+      ->setEmail("admin@bilemo.com")
+      ->setPassword($this->passwordHashe->hashPassword($user, "admin"));
+
+    $manager->persist($user);
+    $manager->flush();
+  }
 }
