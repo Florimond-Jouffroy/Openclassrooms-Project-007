4d0e374db9a556316a0e73d85cd08bd199ba143c
Florimond Jouffroy <florimond.jouffroy@gmail.com>
2022-07-06 11:44:37 +0200

    feat: getAllMobiles

---
+++ src/Controller/Mobile/AllController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller\Mobile;
+
+use App\Repository\MobileRepository;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class AllController extends AbstractController
+{
+
+  #[Route('/api/mobiles', name: 'mobile_all', methods: ['GET'])]
+  public function getAllMobiles(MobileRepository $mobileRepository, SerializerInterface $serializer)
+  {
+    $mobileList = $mobileRepository->findAll();
+    $jsonMobileList = $serializer->serialize($mobileList, 'json');
+
+    return new JsonResponse($jsonMobileList, Response::HTTP_OK, [], true);
+  }
+}
